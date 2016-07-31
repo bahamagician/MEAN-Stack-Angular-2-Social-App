4c97db5f2e4b1a67016b49c8325f4fd1c3816638
Chris Thronebury <cthronebury@gmail.com>
2016-07-31 12:16:40 -0400

    Create message service

---
+++ assets/app/messages/message.service.ts
@@ -0,0 +1,15 @@
+import {Message} from "./message";
+export class MessageService{
+    messages: Message[] = [];
+
+    addMessage(message: Message) {
+        this.messages.push(message);
+    }
+
+    getMessages(){
+        return this.messages;
+    }
+    deleteMessage(message: Message) {
+        this.messages.splice(this.messages.indexOf(message), 1);
+    }
+}
