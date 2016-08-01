d63ae543555b08a9a79c3cde459b73ec3190dc49
Chris Thronebury <cthronebury@gmail.com>
2016-07-31 23:32:44 -0400

    Add routing and refactor components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,20 +1,23 @@
 import {Component} from '@angular/core';
-import {MessageListComponent} from "./messages/message-list.component";
-import {MessageInputComponent} from "./messages/message-input.component";
+import {HeaderComponent} from "./header.component";
+import {Routes, ROUTER_DIRECTIVES} from "@angular/router";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
 
 @Component({
     selector: 'my-app',
-    template: ` 
-       
-        <div class="row spacing">
-          <my-message-input></my-message-input>
-        </div>
-        
-        <div class="row spacing">
-          <my-message-list></my-message-list>
-        </div>                
+    template: `
+      <div class="container">
+        <my-header></my-header>
+      </div>
+      <router-outlet></router-outlet>
     `,
-    directives: [MessageListComponent, MessageInputComponent]
+    directives: [HeaderComponent, ROUTER_DIRECTIVES]
+
 })
+@Routes([
+    {path: '/', component: MessagesComponent},
+    {path: '/auth', component: AuthenticationComponent}
+])
 export class AppComponent {
 }
--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -1,7 +1,7 @@
 ///<reference path="../../typings.d.ts"/>
 import { bootstrap } from '@angular/platform-browser-dynamic';
-
 import { AppComponent } from "./app.component";
 import {MessageService} from "./messages/message.service";
+import {ROUTER_PROVIDERS} from "@angular/router";
 
-bootstrap(AppComponent, [MessageService]);
+bootstrap(AppComponent, [MessageService, ROUTER_PROVIDERS]);
