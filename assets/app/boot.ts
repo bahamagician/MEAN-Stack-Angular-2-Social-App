e30d55fa8aaef6dc3c3825f0a503d802d7faa8be
Chris Thronebury <cthronebury@gmail.com>
2016-07-31 23:40:28 -0400

    Add hash location strategy

--- assets/app/boot.ts
+++ assets/app/boot.ts
@@ -3,5 +3,7 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from "./app.component";
 import {MessageService} from "./messages/message.service";
 import {ROUTER_PROVIDERS} from "@angular/router";
+import {provide} from "@angular/core";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 
-bootstrap(AppComponent, [MessageService, ROUTER_PROVIDERS]);
+bootstrap(AppComponent, [MessageService, ROUTER_PROVIDERS, provide(LocationStrategy, {useClass: HashLocationStrategy})]);
